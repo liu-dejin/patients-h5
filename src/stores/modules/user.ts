cef5b8b5d3975fc52c939202276b7457e5656234
Your Name <liudejin@kis2.cn>
2025-07-14 21:46:45 +0800

    pinia配置自定义

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -5,19 +5,36 @@ import { ref } from 'vue'
 /**
  * 用户store
  */
-export const useUserStore = defineStore('cp-ser', () => {
-  // 全局变量1:登录用户
-  // 空对象变量通过as断言指定类型
-  const user = ref<User>({} as User)
-  // 方法:
-  // 1.存储用户信息
-  const setUser = (userInfo: User) => {
-    user.value = userInfo
-  }
-  // 2.删除用户信息
-  const removeUser = () => {
-    user.value = {} as User
-  }
+export const useUserStore = defineStore(
+  'cp-ser',
+  () => {
+    // 全局变量1:登录用户
+    // 空对象变量通过as断言指定类型
+    const user = ref<User>({} as User)
+    // 方法:
+    // 1.存储用户信息
+    const setUser = (userInfo: User) => {
+      user.value = userInfo
+    }
+    // 2.删除用户信息
+    const removeUser = () => {
+      user.value = {} as User
+    }
 
-  return { user, setUser, removeUser }
-})
+    // 测试
+    const ldj = ref(1)
+
+    return { user, setUser, removeUser, ldj }
+  },
+  //方式1.默认存储所有数据 以store的ID作为存储key
+  // {
+  //   persist: true
+  // }
+  // 方式2.自定义存储key ,指定数据
+  {
+    persist: {
+      key: 'cp-user-app',
+      pick: ['user']
+    }
+  }
+)
