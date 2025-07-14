b67db875d3f4c218927d201acbd0254a0986e593
Your Name <liudejin@kis2.cn>
2025-07-14 20:35:32 +0800

    引入Vant组件 自动按需导入

--- src/test/index.vue
+++ src/test/index.vue
@@ -15,6 +15,16 @@ const setUser = () => {
 <template>
   <div>{{ store.user }}</div>
   <button @click="setUser">设置用户</button>
+  <br />
+  <br />
+  <br />
+  <br />
+  <hr />
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
 </template>
 
 <style lang="less" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 按需导入vant 自动注册导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // vant按需导入 插件
+    AutoImport({
+      resolvers: [VantResolver()]
+    }),
+    Components({
+      dts: false,
+      resolvers: [VantResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
