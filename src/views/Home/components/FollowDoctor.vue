813feb4dd2561f1841753f1692010cd2d7035de1
Your Name <liudejin@kis2.cn>
2025-07-17 20:17:52 +0800

    feat: 添加 @vueuse/core 并优化 FollowDoctor 组件
    - 在 package.json 中添加 @vueuse/core 依赖
    - 更新 pnpm-lock.yaml 文件
    - 在 FollowDoctor 组件中使用 Vue 的生命周期钩子来动态设置窗口宽度
    - 调整 swipe 组件的宽度以适应不同屏幕尺寸

--- src/views/Home/components/FollowDoctor.vue
+++ src/views/Home/components/FollowDoctor.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
 import DoctorCard from './DoctorCard.vue'
+// import { useWindowSize } from '@vueuse/core'
+// const { width } = useWindowSize()
+// 组件初始化拿到设备宽度,页面尺寸改变时,组件会重新计算宽度
+const width = ref(0)
+const setWidth = () => (width.value = window.innerWidth)
+onMounted(() => {
+  setWidth()
+  window.addEventListener('resize', setWidth)
+})
+onUnmounted(() => {
+  window.removeEventListener('resize', setWidth)
+})
 </script>
 
 <template>
@@ -10,7 +23,11 @@ import DoctorCard from './DoctorCard.vue'
     </div>
     <div class="body">
       <!-- swipe 组件 宽度和卡片一样大  不显示指示器 不循环 -->
-      <van-swipe :width="150" :show-indicators="false" :loop="false">
+      <van-swipe
+        :width="(150 / 375) * width"
+        :show-indicators="false"
+        :loop="false"
+      >
         <van-swipe-item v-for="i in 5" :key="i">
           <doctor-card></doctor-card>
         </van-swipe-item>
